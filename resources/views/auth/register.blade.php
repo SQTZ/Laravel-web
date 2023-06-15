f74265d717f053650a56f486b5a1c9111f789d2e
SQTZ <pierre.legouix76@gmail.com>
2023-06-15 10:43:57 +0200

    Updated English -> French Mode

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,23 +4,23 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-label for="name" :value="__('Prénom')" />
+            <x-text-input id="name" class="h-10 px-4 bg-gray-700 text-gray-300 focus:outline-none rounded-md border-2 border-blue-500 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="h-10 px-4 bg-gray-700 text-gray-300 focus:outline-none rounded-md border-2 border-blue-500 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="h-10 px-4 bg-gray-700 text-gray-300 focus:outline-none rounded-md border-2 border-blue-500 w-full"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
@@ -30,9 +30,9 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmez votre mot de passe')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password_confirmation" class="h-10 px-4 bg-gray-700 text-gray-300 focus:outline-none rounded-md border-2 border-blue-500 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
@@ -41,11 +41,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Déjà inscrit ?') }}
             </a>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
+            <x-primary-button class="ml-4 border-2 border-blue-500 text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-500 hover:text-white duration-150">
+                {{ __('Créer mon compte') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,18 +1,18 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Delete Account') }}
+            {{ __('Supprimer mon compte') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Cette action est irréversible. Une fois que votre compte est supprimé, il ne peut pas être restauré, et vous ne pourrez plus accéder à aucune donnée ou information associée à votre compte.') }}
         </p>
     </header>
 
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('Supprimer') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
@@ -20,22 +20,22 @@
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('Êtes vous sur ?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('Cette action est irréversible. Une fois que votre compte est supprimé, il ne peut pas être restauré, et vous ne pourrez plus accéder à aucune donnée ou information associée à votre compte. Si vous comprenez les conséquences de cette action et souhaitez toujours supprimer votre compte, veuillez confirmer votre décision. Autrement, si vous souhaitez conserver votre compte, vous pouvez simplement fermer cette page ou naviguer ailleurs sur notre site. Nous vous recommandons de sauvegarder toutes les informations ou données importantes avant de procéder à la suppression de votre compte.Nous ne serons pas en mesure de récupérer ou de fournir aucune information après que le compte a été supprimé.') }}
             </p>
 
             <div class="mt-6">
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                <x-input-label for="password" value="{{ __('Mot de passe') }}" class="sr-only" />
 
                 <x-text-input
                     id="password"
                     name="password"
                     type="password"
-                    class="mt-1 block w-3/4"
-                    placeholder="{{ __('Password') }}"
+                    class="h-10 px-4 bg-gray-700 text-gray-300 focus:outline-none rounded-md border-2 border-blue-500 w-full"
+                    placeholder="{{ __('Mot de passe') }}"
                 />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
@@ -43,11 +43,11 @@
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
+                    {{ __('Annuler') }}
                 </x-secondary-button>
 
                 <x-danger-button class="ml-3">
-                    {{ __('Delete Account') }}
+                    {{ __('Supprimer définitivement') }}
                 </x-danger-button>
             </div>
         </form>
