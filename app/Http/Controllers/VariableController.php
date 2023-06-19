e35b5dbe8efa583303fbc87f9f01db1a0469db20
SQTZ <pierre.legouix76@gmail.com>
2023-06-19 11:06:37 +0200

    Updated function UpdateOrCreate

--- app/Http/Controllers/VariableController.php
+++ app/Http/Controllers/VariableController.php
@@ -16,28 +16,38 @@ class VariableController extends Controller
 
 
     public function deploy(Request $request)
-    {
-        //Je récupère les données du formulaire
-        $Taux_horaire = $request->input('Taux_horaire');
-        $Cout_ff = $request->input('Cout_ff');
-        $Cout_bobine = $request->input('Cout_bobine');
+{
+    //Je récupère les données du formulaire
+    $Taux_horaire = $request->input('Taux_horaire');
+    $Cout_ff = $request->input('Cout_ff');
+    $Cout_bobine = $request->input('Cout_bobine');
 
-        //je met une condition, si les données sont vides, je renvoie un message d'erreur
-        if (empty($Taux_horaire) || empty($Cout_ff) || empty($Cout_bobine)) {
-            return response()->json(['error' => 'Veuillez remplir tous les champs.']);
-        }
+    //je met une condition, si les données sont vides, je renvoie un message d'erreur
+    if (empty($Taux_horaire) || empty($Cout_ff) || empty($Cout_bobine)) {
+        return response()->json(['error' => 'Veuillez remplir tous les champs.']);
+    }
 
-        //J'envoie les données dans la base de données
+    if (G_variable::count() > 0) {
+        // Si la table a déjà des données, on met à jour toutes les lignes
+        $data = G_variable::query()->update([
+            'Taux_horaire' => $Taux_horaire,
+            'Cout_ff' => $Cout_ff,
+            'Cout_bobine' => $Cout_bobine,
+        ]);
+    } else {
+        // Si la table est vide, on crée une nouvelle ligne
         $data = G_variable::create([
             'Taux_horaire' => $Taux_horaire,
             'Cout_ff' => $Cout_ff,
             'Cout_bobine' => $Cout_bobine,
         ]);
-
-        if ($data) {
-            return response()->json($data);
-        } else {
-            return response()->json(['error' => "J'arrive pas à l'envoyer"]);
-        }
     }
+
+    if ($data) {
+        return response()->json($data);
+    } else {
+        return response()->json(['error' => "J'arrive pas à l'envoyer"]);
+    }
+}
+
 }
